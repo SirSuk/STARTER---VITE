c8e2933afdc1631d03a35f5e0b285dc2714445c0
Radif El noun <SIRIOSUKUR@GMAIL.COM>
2023-01-07 14:13:25 +0100

    jokesgenerator

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+
+const btn = document.querySelector('.btn')
+const section = document.querySelector('.section')
+
+
 console.log("proyect started")
+
+function pintaDatos(datos){
+  console.log(datos.joke)
+  const newJoke =document.createElement('article');
+  console.log(newJoke)
+  newJoke.className = 'chiste';
+
+  newJoke.innerHTML = `
+  <img src="./src/jokeface.jpg" alt="">
+  <p>${datos.joke}</p>
+  `
+
+  section.appendChild(newJoke);
+}
+
+btn.onclick = () => {
+
+  fetch('https://v2.jokeapi.dev/joke/Any?type=single')
+    .then((resp) => resp.json() )
+    .then((data) => { pintaDatos(data) })
+    .catch((err) => console.error(err))
+
+}
